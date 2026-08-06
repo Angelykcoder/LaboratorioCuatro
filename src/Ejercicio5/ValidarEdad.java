aade0f14af198be11b3a0021588cc065a7d224da
Angelykcoder <dfloresg11@miumg.edu.gt>
2026-08-05 22:35:16 -0600

    Tome codigo para crear ejercicio 6

--- src/Ejercicio5/ValidarEdad.java
+++ src/Ejercicio5/ValidarEdad.java
@@ -17,8 +17,7 @@ public class ValidarEdad {
   System.out.print("Por favor, ingrese su edad: ");
             
             try {
-                // Leemos la entrada como texto y luego la convertimos a entero (int).
-                // Esto evita problemas en el buffer del Scanner cuando el usuario presiona Enter.
+               
                 String entrada = scanner.nextLine();
                 edad = Integer.parseInt(entrada);
                 
@@ -30,16 +29,16 @@ public class ValidarEdad {
                      throw new IllegalArgumentException("La edad debe ser mayor a cero.");
                 }
                 
-                // Si el código llega a esta línea, significa que no hubo errores ni excepciones
+   
                 edadValida = true;
                 System.out.println("\n¡Registro exitoso! Edad guardada: " + edad + " años.");
                 
             } catch (NumberFormatException e) {
-                // Este bloque atrapa el error si el usuario ingresa letras, decimales o símbolos
+           
                 System.out.println(" Error: Debe ingresar un número entero válido (Ejemplo: 25). Inténtelo de nuevo.\n");
                 
             } catch (IllegalArgumentException e) {
-                // Este bloque atrapa el error que nosotros mismos lanzamos si el número es negativo o cero
+            
                 System.out.println(" Error: " + e.getMessage() + " Inténtelo de nuevo.\n");
             }
         }
